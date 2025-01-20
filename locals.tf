a47427bc1bd58663c8341814aa6d6743c51f9653
Craig Sloggett <craigsloggett@gmail.com>
2025-01-20 13:13:02 -0500

    chore: Add all IDs to the locals.tf file
    Signed-off-by: Craig Sloggett <craigsloggett@gmail.com>

--- locals.tf
+++ locals.tf
@@ -1,10 +1,19 @@
 locals {
-  organization_membership_ids = {
-    # Hidden internal users for HashiCorp's systems.
+  owners_team_id     = "team-p9ryLNrpQ9TNmEFh"
+  default_project_id = "prj-1ifijXyay7xeH9Zc"
+
+  # The TFE Provider Authentication variable set.
+  tfe_provider_authentication_variable_set_id           = "varset-FwFGK65suTr7eaAA"
+  tfe_provider_authentication_workspace_variable_set_id = "craigsloggett-lab/hcp-terraform-admin/TFE Provider Authentication"
+
+  # The resources defined in `backend.tf`.
+  hcp_terraform_admin_workspace_id = "ws-aAUcuGF6QEJKuQyL"
+  hcp_platform_team_project_id     = "prj-qW7NitPW1R49WN4b"
+
+  # Hidden users for HashiCorp's internal systems.
+  hidden_users_organization_membership_ids = {
     api_org     = "ou-EhS6WkPBDVPxYYqd"
     api_team    = "ou-mLy6EZTkP2bKW9w3"
     gh_webhooks = "ou-j7QUTL6CcJUktJJC"
-    # Doormat is the service used to manage HCP Terraform organizations for HashiCorp employees.
-    doormat = "ou-9KRr8gq4h6JikktE"
   }
 }
